432aaa79b0576f1d80a74cba5f2105b035245bea
meyhem <meyhem@protonmail.com>
2026-04-02 20:34:41 +0200

    Add click impulse logic for balls towards click position

--- scenes/root.gd
+++ scenes/root.gd
@@ -4,6 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
+	set_process_input(true)
 	var ball_scene = preload("res://scenes/ball.tscn")
 	var screen_size = get_viewport().get_visible_rect().size
 	for i in range(20):
@@ -17,3 +18,16 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		var click_pos_viewport = get_viewport().get_mouse_position()
+		var camera = get_viewport().get_camera_2d()
+		var click_pos_world = camera.get_viewport_transform().affine_inverse() * click_pos_viewport
+		for child in get_children():
+			if child is RigidBody2D:
+				var direction = (click_pos_world - child.global_position).normalized()
+				var impulse_strength = 1000.0
+				var impulse = direction * impulse_strength
+				child.apply_central_impulse(impulse)
